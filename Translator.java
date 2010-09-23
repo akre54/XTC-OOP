7abe41a38ae70d762fbe5bc715bed4741d79e3db
Patrick Hammer <pathammer@gmail.com>
2010-09-23 15:58:34 -0400

    Updated the Translator.java copywrite
    I did this mostly to test out git, not because I care.

--- Translator.java
+++ Translator.java
@@ -1,6 +1,7 @@
 /*
  * Object-Oriented Programming
  * Copyright (C) 2010 Robert Grimm
+ * Edited by Patrick Hammer
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
