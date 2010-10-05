455d17354fc74245f340d7b78d38ed31393ab6d8
Patrick Hammer <pathammer@gmail.com>
2010-10-05 19:41:25 -0400

    Added group members names to the (C) and @author tags. Added -translate option to xtc.oop.Translator. Added note on how to procede from here. Updated foo.java to be translatable.

--- Translator.java
+++ Translator.java
@@ -2,7 +2,6 @@
  * Object-Oriented Programming
  * Copyright (C) 2010 Robert Grimm
  * Edited by Patrick Hammer
-
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -38,96 +37,108 @@ import xtc.util.Tool;
 /**
  * A translator from (a subset of) Java to (a subset of) C++.
  *
- * @author Robert Grimm
+ * @author P.Hammer
+ * @author A.Krebs
+ * @author L. Pelka
+ * @author P.Ponzeka
  * @version $Revision$
  */
 public class Translator extends Tool {
 
-  /** Create a new translator. */
-  public Translator() {
-    // Nothing to do.
-  }
+	/** Create a new translator. */
+	public Translator() {
+		// Nothing to do.
+	}
 
-  public String getCopy() {
-    return "(C) 2010 <Group Name>";
-  }
+	public String getCopy() {
+		return "(C) 2010 P.Hammer, A.Krebs, L. Pelka, P.Ponzeka";
+	}
 
-  public String getName() {
-    return "Java to C++ Translator";
-  }
+	public String getName() {
+		return "Java to C++ Translator";
+	}
 
-  public String getExplanation() {
-    return "This tool translates a subset of Java to a subset of C++.";
-  }
+	public String getExplanation() {
+		return "This tool translates a subset of Javat to a subset of C++.";
+	}
 
-  public void init() {
-    super.init();
+	public void init() {
+		super.init();
 
-    // Declare command line arguments.
-    runtime.
-      bool("printJavaAST", "printJavaAST", false,
-           "Print the Java AST.").
-      bool("countMethods", "optionCountMethods", false,
-           "Print the number of method declarations.");
-  }
+		// Declare command line arguments.
+		runtime.
+			bool("printJavaAST", "printJavaAST", false,
+				 "Print the Java AST.").
+			bool("countMethods", "optionCountMethods", false,
+				 "Print the number of method declarations.").
+			bool("translate", "translate", false,
+				 "Translate .java file to c++.");
+	}
 
-  public void prepare() {
-    super.prepare();
+	public void prepare() {
+		super.prepare();
 
-    // Perform consistency checks on command line arguments.
-  }
+		// Perform consistency checks on command line arguments.
+	}
 
-  public File locate(String name) throws IOException {
-    File file = super.locate(name);
-    if (Integer.MAX_VALUE < file.length()) {
-      throw new IllegalArgumentException(file + ": file too large");
-    }
-    return file;
-  }
+	public File locate(String name) throws IOException {
+		File file = super.locate(name);
+		if (Integer.MAX_VALUE < file.length()) {
+			throw new IllegalArgumentException(file + ": file too large");
+		}
+		return file;
+	}
 
-  public Node parse(Reader in, File file) throws IOException, ParseException {
-    JavaFiveParser parser =
-      new JavaFiveParser(in, file.toString(), (int)file.length());
-    Result result = parser.pCompilationUnit(0);
-    return (Node)parser.value(result);
-  }
+	public Node parse(Reader in, File file) throws IOException, ParseException {
+		JavaFiveParser parser =
+			new JavaFiveParser(in, file.toString(), (int)file.length());
+		Result result = parser.pCompilationUnit(0);
+		return (Node)parser.value(result);
+	}
 
-  public void process(Node node) {
-    // Handle the printJavaAST option
-    if (runtime.test("printJavaAST")) {
-      runtime.console().format(node).pln().flush();
-    }
+	public void process(Node node) {
+		// Handle the printJavaAST option
+		if (runtime.test("translate")) {
+			runtime.console().p("translating...").pln().flush();
+			/* to do list:
+			 * make .cpp file 
+			 * walk the tree translating the same way as is done below
+			 */
+		}
+		if (runtime.test("printJavaAST")) {
+			runtime.console().format(node).pln().flush();
+		}
 
-    if (runtime.test("optionCountMethods")) {
-      new Visitor() {
-        int count = 0;
+		if (runtime.test("optionCountMethods")) {
+			new Visitor() {
+				int count = 0;
 
-        public void visitCompilationUnit(GNode n) {
-          visit(n);
-          runtime.console().p("Number of methods: ").p(count).pln().flush();
-        }
+				public void visitCompilationUnit(GNode n) {
+					visit(n);
+					runtime.console().p("Number of methods: ").p(count).pln().flush();
+				}
 
-        public void visitMethodDeclaration(GNode n) {
-          count++;
-          visit(n);
-        }
+				public void visitMethodDeclaration(GNode n) {
+					count++;
+					visit(n);
+				}
 
-        public void visit(Node n) {
-          for (Object o : n) if (o instanceof Node) dispatch((Node)o);
-        }
+				public void visit(Node n) {
+					for (Object o : n) if (o instanceof Node) dispatch((Node)o);
+				}
 
-      }.dispatch(node);
-    }
+			}.dispatch(node);
+		}
 
-  }
+	}
 
-  /**
-   * Run the translator with the specified command line arguments.
-   *
-   * @param args The command line arguments.
-   */
-  public static void main(String[] args) {
-    new Translator().run(args);
-  }
+	/**
+	 * Run the translator with the specified command line arguments.
+	 *
+	 * @param args The command line arguments.
+	 */
+	public static void main(String[] args) {
+		new Translator().run(args);
+	}
 
 }
--- foo.java
+++ foo.java
@@ -1,42 +1,8 @@
 
-//COMPILING & RUNNING: javac Main.java && java Main < input.txt
-
-import java.util.*;
+//Comment
 
 public class Main {
-
-    public static Scanner in;
-
     public static void main(String[] args) {
-        in = new Scanner(System.in);
-        doStuff();
+        int x = 0;
     }
-
-    public static void doStuff() {
-        int NumberOfInputs = in.nextInt();
-        int Queens;
-        for(int count=1;count<=NumberOfInputs;count++){
-            int N=in.nextInt();
-            Queens = (int)(Math.floor((2 * N + 1)/3));
-            System.out.println(count+" "+N+" "+Queens);
-            QueenSolver(N,Queens);
-			System.out.println("");
-        }
-    }
-
-    public static void QueenSolver(int N, int Queens) {
-        int Row=N-Queens+1;
-        int Column = 1;
-        for (int count = 1; count <= Queens; count++) {
-            System.out.print("[" + Row + "," + Column + "] ");
-            if ((count%8)==0){
-                System.out.println("");
-            }
-            Row++;
-            Column=Column+2;
-            if (Column > Row) {
-                Column = 2;
-            }
-    }
-}
 }
