5e633a86320883fa6dd7b7e7093998dc9ce908ef
Patrick Hammer <pathammer@gmail.com>
2010-11-12 10:08:45 -0500

    Empty EWalk class, minor changes elsewhere. Package translate will containe special-case translations.

---
+++ EWalk.java
@@ -0,0 +1,34 @@
+/*
+ * Object-Oriented Programming
+ * 2010
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+package oop;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+import xtc.oop.translate.*;
+
+/**
+ * EWalk is designed to do an in-place translation from java to C++ code that does not literally translate.
+ *
+ * @author P.Hammer
+ * @version v1.0
+ */
+class EWalk {
+	
+}
