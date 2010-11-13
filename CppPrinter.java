acddf0cca286db91fc1c4136f5a42f92197c5d5b
Paige Ponzeka <paigepon@gmail.com>
2010-11-13 12:47:41 -0500

    organized cppPrinter for better readability

--- CppPrinter.java
+++ CppPrinter.java
@@ -30,14 +30,7 @@ import xtc.tree.Node;
 import xtc.tree.Visitor;
 
 import xtc.util.Tool;
-/*To Include:
- 
- CastExpression ??? 
- PrimaryExpression ??
- 
- AllocationExpression ???
- StatementExpression??
- */
+
 public class CppPrinter extends Visitor{
 	private StringBuilder printer;
 	public final boolean DEBUG = false;
@@ -63,17 +56,19 @@ public class CppPrinter extends Visitor{
 		dispatch(call);
 	*/
 	}
-	public void AssetStatement(GNode n)
+	/***********************Expressions***********************/
+	public void visitConditionalExpression(GNode n)
 	{
-		printer.append("assert ");
-		Node express=n.getNode(0);
+		printer.append("(");
+		Node express = n.getNode(0);
 		dispatch(express);
-		printer.append("[ :");
-		Node express2=n.getNode(1);
-		dispatch(express2);
-		printer.append("]; ");
-		
-	}
+		printer.append(") ? ");
+		Node one = n.getNode(1);
+		dispatch(one);
+		printer.append(" : ");
+		Node two = n.getNode(2);
+		dispatch(two);
+	}	
 	public void visitSelectionExpression(GNode n)
 	{
 		Node prim = n.getNode(0);
@@ -88,93 +83,20 @@ public class CppPrinter extends Visitor{
 			dispatch((Node) o);
 		}
 	}
-	public void visitConditionalExpression(GNode n)
+	public void visitRelationalExpression(GNode n)
 	{
-		printer.append("(");
-		Node express = n.getNode(0);
-		dispatch(express);
-		printer.append(") ? ");
-		Node one = n.getNode(1);
-		dispatch(one);
-		printer.append(" : ");
-		Node two = n.getNode(2);
-		dispatch(two);
+		setBinary(n);		
 	}
-	public void visitSychronizedStatement(GNode n)
+	public void visitEqualityExpression(GNode n)
 	{
-		printer.append("sychronized (");
-		Node expression=n.getNode(0);
-		dispatch(expression);
-		printer.append("){\n");
-		Node block = n.getNode(1);
-		dispatch(block);
-		printer.append("}\n");
-		
-	}
-	public void visitLabeledStatement(GNode n)
+		printer.append("(");		
+		setBinary(n);		
+		printer.append("){ \n");
+	}//end of visitCallExpression method
+	public void visitExpression(GNode n)
 	{
-		printer.append(n.getString(0)+":\n");
-		for(int i=1;i<n.size();i++)
-		{
-			Object o=n.get(i);
-			if(o instanceof String)
-			{
-			}
-			else if(o instanceof Node)
-			{
-				dispatch((Node) o);
-			}
-		}	
-	}
-	public void visitNewClassExpression(GNode n)
-	{
-		printer.append("new ");
-		for(int i=0;i<n.size();i++)
-		{
-			if (i==3) {
-				printer.append("(");
-			}
-			Object o=n.get(i);
-			if(o instanceof String)
-			{
-				printer.append(" "+(String)o);
-			}
-			else if(o instanceof Node)
-			{
-				dispatch((Node) o);
-			}
-			if (i==3) {
-				printer.append(")");
-			}			
-		}
-	}
-	public void visitArguments(GNode n)
-	{
-		for (int i=0; i<n.size(); i++) {
-			
-			dispatch(n.getNode(i));
-			if(i!=(n.size()-1))
-			{
-				printer.append(", ");
-			}
-		}
-	}
-	public void visitFormalParameter(GNode n)
-	{
-		for(int i=0;i<n.size();i++)
-		{
-			Object o=n.get(i);
-			if(o instanceof String)
-			{
-				printer.append(" "+(String)o);
-			}
-			else if(o instanceof Node)
-			{
-				dispatch((Node) o);
-			}
-		}
-	}
-	public void visitUnaryExpression(GNode n)
+		setBinary(n);
+	}	public void visitUnaryExpression(GNode n)
 	{
 		setUnary(n);
 	}
@@ -218,6 +140,65 @@ public class CppPrinter extends Visitor{
 	{
 		setBinary(n);
 	}
+	public void visitExpression(Node n)
+	{
+		Node b=n.getNode(3);
+		dispatch(b);
+	}
+	public void visitAdditiveExpression(GNode n)
+	{
+		setBinary(n);
+	}
+	public void visitMultiplicativeExpression(GNode n)
+	{
+		setBinary(n);
+	}	/*******************Statements ******************************/
+	public void AssertStatement(GNode n)
+	{
+		printer.append("assert ");
+		Node express=n.getNode(0);
+		dispatch(express);
+		printer.append(":");
+		Node express2=n.getNode(1);
+		dispatch(express2);
+		printer.append("; \n");
+	}
+	public void visitSychronizedStatement(GNode n)
+	{
+		printer.append("sychronized (");
+		Node expression=n.getNode(0);
+		dispatch(expression);
+		printer.append("){\n");
+		Node block = n.getNode(1);
+		dispatch(block);
+		printer.append("}\n");
+		
+	}
+	public void visitLabeledStatement(GNode n)
+	{
+		printer.append(n.getString(0)+":\n");
+		for(int i=1;i<n.size();i++)
+		{
+			Object o=n.get(i);
+			if(o instanceof String)
+			{
+			}
+			else if(o instanceof Node)
+			{
+				dispatch((Node) o);
+			}
+		}	
+	}
+	public void visitBreakStatement(GNode n)
+	{
+		printer.append("break");
+		setBreCon(n);
+	}
+	public void visitContinueStatement(GNode n)
+	{
+		printer.append("continue");
+		setBreCon(n);
+	}
 	public void visitTryCatchFinallyStatement(GNode n)
 	{
 		
@@ -229,7 +210,6 @@ public class CppPrinter extends Visitor{
 			Node catch1 = n.getNode(i);
 			dispatch(catch1);
 		}
-		
 	}
 	public void visitThrowStatement(GNode n)
 	{
@@ -237,26 +217,86 @@ public class CppPrinter extends Visitor{
 		visit(n);
 		printer.append(";\n}\n");
 	}
-	public void visitCatchClause(GNode n)
-	{
-		printer.append("catch(");
-		Node io = n.getNode(0);
-		dispatch(io);
-		printer.append("){\n");
-		Node block = n.getNode(1);
-		dispatch(block);
-	}
 	public void visitReturnStatement(GNode n)
 	{
 		printer.append("return ");
 		visit(n);
 		printer.append("; \n");
 	}
-	public void visitExpression(Node n)
+	
+
+	public void visitExpressionStatement(GNode n)
 	{
-		Node b=n.getNode(3);
-		dispatch(b);
-		//visit(b);
+		visit(n);
+		printer.append(";\n");
+	}
+	public void visitSwitchStatement(GNode n)
+	{
+		printer.append("switch(");
+		Node h= n.getNode(0);
+		dispatch(h);
+		printer.append(")\n");
+		printer.append("{\n");
+		for(int i=1; i<(n.size());i++)
+		{
+			Node cases= n.getNode(i);
+			dispatch(cases);
+		}
+		printer.append("}\n");
+	}
+	public void visitCaseClause(GNode n)
+	{
+		printer.append("case");
+		Node theCase= n.getNode(0);
+		dispatch(theCase);
+		printer.append(": ");
+		Node theExp= n.getNode(1);
+		dispatch(theExp);	
+		Node break1= n.getNode(2);
+		dispatch(break1);	
+	}	
+	public void visitConditionalStatement(GNode n)
+	{
+		printer.append("if(");
+		Node express=n.getNode(0);
+		dispatch(express);
+		printer.append("){\n");
+		Node block = n.getNode(1);
+		dispatch(block);
+		printer.append("}\n");
+		for(int i=2;i<n.size();i++)
+		{
+			printer.append("else ");
+			Node cond =n.getNode(i);
+			if(cond.getName().equals("Block")){
+				printer.append("{ \n");
+				dispatch(cond);
+				printer.append("} \n");
+			}
+			else
+			{
+				dispatch(cond);
+			}
+			
+		}
+		
+	}	
+	////////////////Loops////////////////////////
+	public void visitDoWhileStatement(GNode n)
+	{
+		printer.append("do{\n");
+		Node block= n.getNode(0);
+		dispatch(block);
+		printer.append("}while(");
+		Node expression= n.getNode(1);
+		dispatch(expression);
+		printer.append(");\n");
+	}
+	public void visitWhileStatement(GNode n)
+	{
+		printer.append("\n while");
+		visit(n);
+		printer.append("} \n");
 	}
 	public void visitForUpdate(Node n)
 	{
@@ -292,82 +332,71 @@ public class CppPrinter extends Visitor{
 		Node f= n.getNode(1);
 		visit(f);
 		printer.append("}\n");	
-	}
-	public void visitStatement(GNode n)
+	}	/***********************Classes ******************************/
+	public void visitNewClassExpression(GNode n)
 	{
-		//dispatch(n);
-	}
-	public void visitExpressionStatement(GNode n)
-	{
-		visit(n);
-		printer.append(";\n");
-	}
-	
-	public void visitAdditiveExpression(GNode n)
-	{
-		setBinary(n);
-	}
-	public void visitMultiplicativeExpression(GNode n)
-	{
-		setBinary(n);
-	}
-	public void visitSwitchStatement(GNode n)
-	{
-		printer.append("switch(");
-		Node h= n.getNode(0);
-		dispatch(h);
-		printer.append(")\n");
-		printer.append("{\n");
-		for(int i=1; i<(n.size());i++)
+		printer.append("new ");
+		for(int i=0;i<n.size();i++)
 		{
-			Node cases= n.getNode(i);
-			dispatch(cases);
-		}
-		printer.append("}\n");
-	}
-	public void visitCaseClause(GNode n)
-	{
-		printer.append("case");
-		Node theCase= n.getNode(0);
-		dispatch(theCase);
-		printer.append(": ");
-		Node theExp= n.getNode(1);
-		dispatch(theExp);	
-		Node break1= n.getNode(2);
-		dispatch(break1);	
-	}
-	public void visitConditionalStatement(GNode n)
-	{
-		printer.append("if(");
-		Node express=n.getNode(0);
-		dispatch(express);
-		printer.append("){\n");
-		Node block = n.getNode(1);
-		dispatch(block);
-		printer.append("}\n");
-		for(int i=2;i<n.size();i++)
-		{
-			printer.append("else ");
-			Node cond =n.getNode(i);
-			if(cond.getName().equals("Block")){
-				printer.append("{ \n");
-				dispatch(cond);
-				printer.append("} \n");
+			if (i==3) {
+				printer.append("(");
 			}
-			else
+			Object o=n.get(i);
+			if(o instanceof String)
 			{
-				dispatch(cond);
+				printer.append(" "+(String)o);
 			}
-			
+			else if(o instanceof Node)
+			{
+				dispatch((Node) o);
+			}
+			if (i==3) {
+				printer.append(")");
+			}			
 		}
-		
 	}
 	public void visitCallExpression(GNode n)
 	{
 		//printer.append("CALLEXPRESSION");
 		visit(n);
 	}
-	
+	/**********************Other***************************/
+	public void visitArguments(GNode n)
+	{
+		for (int i=0; i<n.size(); i++) {
+			
+			dispatch(n.getNode(i));
+			if(i!=(n.size()-1))
+			{
+				printer.append(", ");
+			}
+		}
+	}
+	public void visitFormalParameter(GNode n)
+	{
+		for(int i=0;i<n.size();i++)
+		{
+			Object o=n.get(i);
+			if(o instanceof String)
+			{
+				printer.append(" "+(String)o);
+			}
+			else if(o instanceof Node)
+			{
+				dispatch((Node) o);
+			}
+		}
+	}
+			
+	public void visitCatchClause(GNode n)
+	{
+		printer.append("catch(");
+		Node io = n.getNode(0);
+		dispatch(io);
+		printer.append("){\n");
+		Node block = n.getNode(1);
+		dispatch(block);
+	}
 	public void visitDefaultClause(GNode n)
 	{
 		printer.append("default");
@@ -377,46 +406,6 @@ public class CppPrinter extends Visitor{
 		Node break1= n.getNode(1);
 		dispatch(break1);	
 		
-	}
-	public void visitBreakStatement(GNode n)
-	{
-		printer.append("break");
-		setBreCon(n);
-	}
-	public void visitContinueStatement(GNode n)
-	{
-		printer.append("continue");
-		setBreCon(n);
-	}
-	public void visitDoWhileStatement(GNode n)
-	{
-		printer.append("do{\n");
-		Node block= n.getNode(0);
-		dispatch(block);
-		printer.append("}while(");
-		Node expression= n.getNode(1);
-		dispatch(expression);
-		printer.append(");\n");
-	}
-	public void visitWhileStatement(GNode n)
-	{
-		printer.append("\n while");
-		visit(n);
-		printer.append("} \n");
-	}
-	public void visitRelationalExpression(GNode n)
-	{
-		setBinary(n);		
-	}
-	public void visitEqualityExpression(GNode n)
-	{
-		printer.append("(");		
-		setBinary(n);		
-		printer.append("){ \n");
-	}//end of visitCallExpression method
-	public void visitExpression(GNode n)
-	{
-		setBinary(n);
 	}
 	public void visitFieldDeclaration(GNode n)
 	{
@@ -467,7 +456,6 @@ public class CppPrinter extends Visitor{
 		for (int i=0; i<n.size(); i++) {
 			Object o=n.get(i);
 			if (o!=null) {
-				//printer.append("(");
 			}
 			if(o instanceof String)
 			{
