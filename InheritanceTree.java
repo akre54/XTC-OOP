51be636bf33cc115a09c7a354a1fd041e7418d84
elizabeth pelka <ejpelka@gmail.com>
2010-12-19 14:44:47 -0500

    fixed local variable problem by adding parameters in

--- InheritanceTree.java
+++ InheritanceTree.java
@@ -24,7 +24,7 @@ public class InheritanceTree{
 	public InheritanceTree superclass;
 	public ArrayList<InheritanceTree> subclasses;
 	
-	boolean VERBOSE = false;
+	boolean VERBOSE = true;
 	
 
 	/**
