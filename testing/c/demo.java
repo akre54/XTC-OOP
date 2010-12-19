061906d414784401b9cfcc6542042529e2bcb14e
Adam Krebs <amk528@cs.nyu.edu>
2010-12-18 21:12:31 -0500

    updated testing/c demo to package

--- testing/c/demo.java
+++ testing/c/demo.java
@@ -10,6 +10,6 @@ public class demo
 	public static void init ()  { 
 		TestA testa = new TestA (4);
 		TestB testb = new TestB (31.3); 
-		System.out.println("Prod = " + (testa.i * testb.x));
+		//System.out.println("Prod = " + (testa.i * testb.x));
 	}
 }
